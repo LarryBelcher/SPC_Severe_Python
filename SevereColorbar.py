12261731557973e50f214436e30e780ed5d44db5
LarryBelcher <larryrbelcher@gmail.com>
2015-02-14 12:05:51 -0500

    Text revisions to the scripts
    Some text revisions to the mapping scripts based on team feedback

--- SevereColorbar.py
+++ SevereColorbar.py
@@ -120,7 +120,9 @@ if(imgsize == '620' or imgsize == '1000' or imgsize == 'DIY'):
 	plt.text(t1x, t1y, dval, fontproperties=propb, size=fsiz1, color='#333333')
 	plt.text(t2x, t2y, "($\%$)", fontproperties=propr, size=fsiz1, color='#333333')
 
-	plt.text(t3x, t3y, mmdd, fontproperties=propr, size=fsiz2, color='#8D8D8D')
+	plt.text(t3x, t3y, mmdd, fontproperties=propb, size=fsiz2, color='#8D8D8D')
+	plt.text(t3x, t3y-0.22, 'Based on storms', fontproperties=propr, size=fsiz2, color='#8D8D8D')
+	plt.text(t3x, t3y-0.44, 'from 1982-2011', fontproperties=propr, size=fsiz2, color='#8D8D8D')
 	plt.text(t4x, t4y, 'Climate.gov', fontproperties=propr, size=fsiz2, color='#8D8D8D')
 	plt.text(t5x, t5y, 'Data: SPC', fontproperties=propr, size=fsiz2, color='#8D8D8D')
 
